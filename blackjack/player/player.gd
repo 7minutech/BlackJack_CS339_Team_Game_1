8aaa2ec62c435afcb5539962611eb7b18885d350
7minutech <elijohnson1107@gmail.com>
2025-02-17 11:00:26 -0600

    feat: added player methods

--- blackjack/player/player.gd
+++ blackjack/player/player.gd
@@ -1,5 +1,10 @@
 extends Node
 
+var cards = []
+var abiliites = []
+var chips = 3
+var total_card_value 
+var selected_cards = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,3 +14,29 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func clear_cards():
+	cards = []
+
+func clear_selected_cards():
+	selected_cards = []
+	
+func has_bust():
+	if total_card_value > 21:
+		return true
+	return false
+	
+func can_split_cards(card1,card2):
+	if card1.value == card1.value && card1.rank == card2.rank && selected_cards.length == 2:
+		return true
+	return false
+	
+func sum_card_value():
+	total_card_value = 0
+	for card in cards:
+		total_card_value += card.value
+	
+func player_lost():
+	if chips <= 0:
+		return true
+	return false
