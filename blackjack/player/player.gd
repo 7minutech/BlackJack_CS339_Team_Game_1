78ebc937addca1b8c42d680cf362f252dde48c83
7minutech <elijohnson1107@gmail.com>
2025-02-17 11:05:05 -0600

    feat: added lose chip method

--- blackjack/player/player.gd
+++ blackjack/player/player.gd
@@ -14,6 +14,9 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+func lose_chip():
+	chips -= 1;
 
 func clear_cards():
 	cards = []
