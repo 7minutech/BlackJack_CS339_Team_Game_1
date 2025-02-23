a36024f20ec1166b1fc1d43a88fc8264205fcac4
7minutech <elijohnson1107@gmail.com>
2025-02-23 15:54:52 -0600

    test: added testing for deck

---
+++ blackjack/Scripts/main.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$Deck.create_deck()
+	$Deck.shuffle()
+	var drawn_card = $Deck.cards.pop_front()
+	print(drawn_card)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
