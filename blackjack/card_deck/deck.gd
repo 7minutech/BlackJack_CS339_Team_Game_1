64a67372b8a59172a3d82900af33b3e9cbc41ffe
7minutech <elijohnson1107@gmail.com>
2025-02-23 15:53:43 -0600

    fix: changed create_deck method

--- blackjack/card_deck/deck.gd
+++ blackjack/card_deck/deck.gd
@@ -1,41 +1,33 @@
 extends Node2D
+
 var card_scene = preload("res://card_deck/card.tscn")
-var card = card_scene.instantiate()
 var cards = []
-var royalty = ["king","queen","jack","ace"]
-var numbers = [2,3,4,5,6,7,8,9,10]
-var suits = ["club","heart","spade","diamond"]
+var royalty = ["jack", "queen", "king", "ace"]
+var numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10]
+var suits = ["club", "heart", "spade", "diamond"]
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	create_deck()
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
 	pass
 
-
 func shuffle():
-	cards.shuffle
-	
+	cards.shuffle()
+
 func create_deck():
-	var rank 
-	var suit
-	var value 
-	for i in len(suits):
-		suit = suits[i]
-		for j in 13:
+	for suit in suits:
+		for j in range(13):
+			var rank
+			var value
+
 			if j < 9:
-				value = numbers[j]	
-				rank = "number"
-			if j > 8:
-				value = 10		
-				rank = royalty[j - 9]
-		add_child(card)
-		card.create_card(value, rank, suit)	
-		cards.append(card)
-		#test rebase
-		card.print_card()
-	
-	
+				value = numbers[j]  # Numbers 2-10
+				rank = "number" # Assing rank to number cards
+			else:
+				value = 10  # Face cards are worth 10
+				rank = royalty[j - 9]  # Assign face card rank (jack, queen, king, ace)
+
+			var card = card_scene.instantiate()
+			card.create_card(value, rank, suit)
+			add_child(card)
+			cards.append(card)
+			print(card)
