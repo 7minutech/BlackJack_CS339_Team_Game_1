53571b7c99824b1c1e7ab7741f6454fdb3aa96cd
Jordan Underwood <junderwood9@stu.jsu.edu>
2025-02-25 21:01:37 -0600

    Main.tscn{Added an abilities.tscn node called AbilityManager}; main.gd{Added some code for testing}; abilities.gd{added functionality for file I/O, creating abilities and populating those abilities with the correct information}; ability.gd{Added toString() function; Added code to the ready function for output; Added new variable cooldown as well as placeholder text for description variable}

--- blackjack/Scripts/ability.gd
+++ blackjack/Scripts/ability.gd
@@ -1,28 +1,35 @@
 extends Node2D
 
 var active: bool
-var description: String
+var description: String = "Placeholder Text"
+var cooldown: int
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	print(self)
+	print()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-#Functions to set and determine if this ability is an active ability or not
-func setActive() -> void:
-	active = true
-func setPassive() -> void:
-	active = false
+#Function to set and determine if this ability is an active ability or not
+func setStatus(status: String) -> void:
+	if (status == "a") or (status == "A"):
+		active = true
+	elif (status == "P") or (status == "P'"):
+		active = false
+
 func isActive() -> bool:
 	return active
 
 #Function to set the description of the ability
 func setDescription(desc: String) -> void:
 	description = desc
+#Function to set the cooldown of the ability
+func setCooldown(cd: int) -> void:
+	cooldown = cd
 
 #Function to set the name of an ability
 func setName(n: String) -> void:
@@ -31,3 +38,6 @@ func setName(n: String) -> void:
 #Function to assign the correct ability sprite
 func setSkin(sprite: CompressedTexture2D) -> void:
 	$Skin.texture = sprite
+
+func _to_string() -> String:
+	return self.name + ": " + self.description + "\n" + "Active = " + str(self.active) + " Cooldown: " + str(self.cooldown)
--- blackjack/Scripts/main.gd
+++ blackjack/Scripts/main.gd
@@ -7,6 +7,10 @@ func _ready() -> void:
 	$Deck.shuffle()
 	var drawn_card = $Deck.cards.pop_front()
 	print(drawn_card)
+	
+	# Code for testing abilities and file management
+	print()
+	$AbilityManager.createSelection()
 	pass # Replace with function body.
 
 
