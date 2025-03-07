2b5e7203b13c05562c245b58ec3c54c89c88e94f
jmcolema45 <100307876+jmcolema45@users.noreply.github.com>
2025-03-06 23:12:02 -0600

    Updated AbilityLogic.gd script

--- blackjack/Scripts/AbilityLogic.gd
+++ blackjack/Scripts/AbilityLogic.gd
@@ -10,6 +10,38 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
+func joker_ability():
+	var player = current_scene.get_node("Player")
+	var dealer = current_scene.get_node("Dealer")
+	if player.total_card_value < 21:
+		var diff = 21 - player.total_card_value
+		var needed_card = dealer.get_card_that_sums_to(diff)
+		player.hit(needed_card)
+		print("Joker Ability: Player's hand adjusted to 21")
+		
+
+func peeping_tom_ability():
+	var dealer = current_scene.get_node("Dealer")
+	var hud = current_scene.get_node("HUD")
+	#show dealer face-down card
+	hud.show_dealers_face_down_card(dealer.face_down_card)
+	print("Peeping Tom Ability: Player can see the dealer face-down card.")
+	
+
+func rewind_time_ability():
+	var player = current_scene.get_node("Player")
+	var dealer = current_scene.get_node("Dealer")
+	var hud = current_scene.get_node("HUD")
+	
+	#check if the last card bust
+	if player.total_card_value > 21:
+		var last_card = player.hand.pop_back()
+		dealer.add_card_to_deck(last_card)
+		hud.update_hand_display(player.hand)
+		current_scene.calculate_total_value()
+		current_scene.display_hands()
+		print("Rewind Time Ability: Last card undone to prevent bust.")
+
 func reroll():
 	var deck = current_scene.get_node("Dealer/Deck")
 	var player = current_scene.get_node("Player")
@@ -31,29 +63,3 @@ func reroll():
 	player.has_bust()
 	var truth: bool = player.bust
 	pass # Replace with function body.
-
-func gambler():
-	var deck = current_scene.get_node("Dealer/Deck")
-	var player = current_scene.get_node("Player")
-	var dealer = current_scene.get_node("Dealer")
-	var hud = current_scene.get_node("HUD")
-	var loweset_value = 100
-	var lowest_index
-	var hand = current_scene.get_node("HUD/Hands")
-	for i in range(player.hand.size()):
-		if loweset_value > player.hand[i].value:
-			loweset_value = player.hand[i].value
-			lowest_index = i
-	var player_lowest_card = player.hand.pop_at(lowest_index)
-	var dealers_face_down = dealer.hand.pop_at(0)
-	dealers_face_down.face_down = false
-	dealers_face_down.flipCard()
-	deck.removeOneFromPlayer(player_lowest_card)
-	deck.removeOneFromDealer(dealers_face_down)
-	hand.addCardToPlayerHand(dealers_face_down)
-	hand.addCardToDealerHand(player_lowest_card)
-	player.hand.append(dealers_face_down)
-	dealer.hand.append(player_lowest_card)
-	
-	
-	
